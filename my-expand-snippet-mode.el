333b973151517845baad0195f2c20db716f2380c
Ole P. Orhagen <ole@orhagen.no>
2025-01-18 12:17:17 +0100

    feat: Pop up a snippet choice buffer on single comma
    That is, the pattern:
    
    ' , |'
    
    Signed-off-by: Ole P. Orhagen <ole@orhagen.no>

--- my-expand-snippet-mode.el
+++ my-expand-snippet-mode.el
@@ -8,11 +8,15 @@
 ;;
 ;;; Real handy for quickly expanding snippets you use a lot.
 ;;
+;;
+;;; - On only comma, call yas-insert-snippet -
+;;
+;;; TODO - When there is no math, raise an error ?
 
 (defun mes--is-xpander-key-p  ()
   "Checks if the preceding word matches the expected <space>,<snippet><space> pattern"
   (interactive)
-  (if (looking-back " ,\\w+ " (point-at-bol))
+  (if (looking-back ",\\w+ " (point-at-bol))
       t
     nil))
 
@@ -26,24 +30,32 @@
         t
       nil)))
 
+(defun mes--is-single-comma ()
+  "Sentinel for the single comma pattern: <space><,><space>"
+  (interactive)
+  (looking-back " , "))
+
 (defun mes-try-n-xpand-word ()
   "Expands the snippet if the previous text matches the pattern as
 set out by mes--is-xpander-key-p, and the word is a yasnippet
 snippet."
   (interactive)
-  (when (and (derived-mode-p 'prog-mode)
-             (mes--is-xpander-key-p)
-             )
-    (save-excursion
-      (backward-char) ;; Back to the beginning of the word
-      (backward-word) ;; Step back over the word itself
-      (backward-char) ;; Back to over the comma
-      (delete-char 1)          ;; Delete the comma
-      (delete-backward-char 1) ;; Delete the space
-      (forward-word))
-    (backward-char)            ;; Back over the space
-    (yas-expand)
-    (evil-insert-state)))      ;; Enter insert
+  (cond ((and (derived-mode-p 'prog-mode)
+              (mes--is-xpander-key-p)
+              )
+         (save-excursion
+           (backward-char)     ;; Back to the beginning of the word
+           (delete-char 1)     ;; Delete the prefix <space>
+           (backward-word)     ;; Step back over the word itself
+           (backward-char)     ;; Back to over the comma
+           (delete-char 1)     ;; Delete the comma
+           (forward-word))
+         (evil-insert-state)       ;; Back over the space
+         (yas-expand)
+         )
+        ((mes--is-single-comma)
+         (delete-backward-char 3)
+         (yas-insert-snippet)))) ;; Enter insert
 
 ;;;###autoload
 (define-minor-mode my-expand-snippet-mode
